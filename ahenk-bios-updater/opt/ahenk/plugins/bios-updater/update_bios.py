7c19fbf11edc2cf5394bdfa42be9dd0e9c99aa83
emre.akkaya <emre.akkaya@agem.com.tr>
2016-06-10 18:49:04 +0300

    impl ahenk update bios task

--- ahenk-bios-updater/opt/ahenk/plugins/bios-updater/update_bios.py
+++ ahenk-bios-updater/opt/ahenk/plugins/bios-updater/update_bios.py
@@ -8,6 +8,7 @@ https://www.python.org/dev/peps/pep-0008/
 
 from base.plugin.abstract_plugin import AbstractPlugin
 from base.model.enum.ContentType import ContentType
+from datetime import datetime
 
 class UpdateBios(AbstractPlugin):
     def __init__(self, data, context):
@@ -20,6 +21,40 @@ class UpdateBios(AbstractPlugin):
     def handle_task(self):
         print('Handling task')
 
+        url = self.data['url']
+        backup_existing = self.data['backupExisting']
+        message = ""
+        code = self.message_code.TASK_PROCESSED.value
+
+        # Backup existing BIOS
+        if backup_existing:
+            filename = "/tmp/bios_{}.bin".format(datetime.now().strftime('%d-%m-%Y_%H:%M:%S'))
+            (result_code, p_out, p_err) = self.execute("flashrom -r {} -p internal".format(filename), shell=True)
+            if result_code == 0:
+                message += "Varolan BIOS, {} dosyasına yedeklendi. ".format(filename)
+                self.logger.debug("Existing BIOS has been read to file: {}".format(filename))
+            else:
+                message += "Varolan BIOS yedeklenirken hata oluştu: {} ".format(str(p_err))
+                code = self.message_code.TASK_ERROR.value
+
+        # Download BIOS file from provided URL
+        (result_code, p_out, p_err) = self.execute("wget {} -O /tmp/newbios.rom".format(url), shell=True)
+        if result_code == 0:
+            # Flash downloaded file
+            # TODO test makinasıyla test edilecek - emre
+            #(result_code, p_out, p_err) = self.execute("flashrom -w /tmp/newbios.rom", shell=True)
+            (result_code, p_out, p_err) = self.execute("ls -altrh /tmp/newbios.rom", shell=True)
+            if result_code == 0:
+                message += "BIOS başarıyla güncellendi. "
+            else:
+                message += "BIOS güncellenirken hata oluştu: {} ".format(str(p_err))
+                code = self.message_code.TASK_ERROR.value
+        else:
+            message += "BIOS dosyası {} adresinden indirilirken hata oluştu: {} ".format(url, str(p_err))
+            code = self.message_code.TASK_ERROR.value
+
+        self.context.create_response(code=code, message=message,
+                                     content_type=ContentType.APPLICATION_JSON.value)
 
 def handle_task(task, context):
     print('Task Data : {}'.format(str(task)))
