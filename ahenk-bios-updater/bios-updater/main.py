05248fe83e009e20c274d94f03c77fa76ea3a487
Volkan Şahin <bm.volkansahin@gmail.com>
2016-10-06 16:44:19 +0300

    main module info added

--- ahenk-bios-updater/bios-updater/main.py
+++ ahenk-bios-updater/bios-updater/main.py
@@ -8,6 +8,9 @@ def info():
     inf['version'] = '1.0.0'
     inf['support'] = 'debian'
     inf['description'] = ''
+    inf['task'] = True
+    inf['user_oriented'] = True
+    inf['machine_oriented'] = True
     inf['developer'] = ''
 
     return inf
